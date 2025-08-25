f5de711ea0d002c1df0d5d492e6780cab473f835
Dddxx98 <deexith2016@gmail.com>
2025-08-25 17:50:19 +0530

    commit 4

--- next/src/app/products/[id]/page.js
+++ next/src/app/products/[id]/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default async function ProductDetails({ params }) {
   const { id } = await params;
   const product = await fetch(`https://dummyjson.com/products/${id}`, { cache: "no-store" });
@@ -10,6 +12,7 @@ export default async function ProductDetails({ params }) {
       <p><strong>Product ID:</strong> {id}</p>
       <h4>{productData.title}</h4>
       <p>{productData.description}</p>
+      <Image src={productData.image} alt={productData.title} width={500} height={500} />
     </section>
   );
 }
